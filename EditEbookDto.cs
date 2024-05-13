ebba7b6497a02aa61446615c16b2a399223c4652
Jairo Calcina <104871195+Broukt@users.noreply.github.com>
2024-05-13 14:21:03 -0400

    feat: add dto to update an ebook

---
+++ EditEbookDto.cs
@@ -0,0 +1,11 @@
+namespace ebooks_dotnet7_api
+{
+    public class EditEbookDto
+    {
+        public string Title { get; set; } = string.Empty;
+        public string Author { get; set; } = string.Empty;
+        public string Genre { get; set; } = string.Empty;
+        public string Format { get; set; } = string.Empty;
+        public int Price { get; set; }
+    }
+}
