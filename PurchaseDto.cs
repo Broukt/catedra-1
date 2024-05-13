3fc1be7cd87f667f1e005a50dbbde3cf88817785
Jairo Calcina <104871195+Broukt@users.noreply.github.com>
2024-05-13 14:50:58 -0400

    feat: add dto that specifies a purchase

---
+++ PurchaseDto.cs
@@ -0,0 +1,9 @@
+namespace ebooks_dotnet7_api
+{
+    public class PurchaseDto
+    {
+        public int Id { get; set; }
+        public int Amount { get; set; }
+        public int Total { get; set; }
+    }
+}
